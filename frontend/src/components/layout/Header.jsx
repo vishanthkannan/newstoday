ca088c13ed0de3b7f73dbb7100b799696eb7cb4a
vishanthannan <vishanthkannan777@gmail.com>
2025-10-05 19:26:28 +0530

    10 - commit

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -220,7 +220,8 @@ const Header = () => {
                 )}
               </div>
             ) : (
-              <div className="hidden md:flex items-center space-x-2">
+              <div className="hidden md:flex items-center space-x-2 flex-shrink-0">
+
                   <Link
                     to="/login"
                     className="text-gray-700 dark:text-gray-300 hover:text-primary-600 dark:hover:text-white px-3 py-2 rounded-md text-sm font-semibold transition-colors"
